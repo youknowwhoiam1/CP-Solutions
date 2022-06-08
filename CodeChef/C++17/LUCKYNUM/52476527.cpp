ca7d86298502091c28431ef8e9901d0c50a5c0f2
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:19 +0530

    Committing files

---
+++ CodeChef/C++17/LUCKYNUM/52476527.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define pb push_back
+#define fi first
+#define se second
+#define max3(a, b, c) max(max((a), (b)), (c))
+#define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
+#define min3(a, b, c) min(min((a), (b)), (c))
+#define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
+using namespace std;
+
+const int mod = 998244353;
+const int inf = 1e17;
+
+int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+
+signed main() {
+
+	NFS;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		char a, b, c;
+		cin >> a >> b >> c;
+		if (a == '7' or b == '7' or c == '7') {
+			cout << "yes" << "\n";
+		} else {
+			cout << "no" << "\n";
+		}
+	}
+
+	return 0;
+}
+// remove online judge snippet for Google Code Jam
