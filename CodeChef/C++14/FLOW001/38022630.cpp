a4378632805a29c3f52c9539348c022fa69f45ba
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:27:02 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW001/38022630.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+     int t;
+     int x;
+     int y;
+     int n;
+     int result;
+     cin >> t;
+     for(n=0; n<t; n++){
+          cin >> x >> y;
+          result = x + y;
+          cout << result << endl;
+     }
+     
+	 return 0;
+}
