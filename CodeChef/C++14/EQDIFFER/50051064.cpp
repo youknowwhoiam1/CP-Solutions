94427e74a7c08695a728220853be9b96531072d2
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:45 +0530

    Committing files

---
+++ CodeChef/C++14/EQDIFFER/50051064.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e17;
+const int N = 1e6 + 1;
+
+// #undef assert
+// #define assert(x) if(!(x))while(true);
+
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+signed main() {
+
+	NFS;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int a[n];
+		map<int, int> m;
+		int mn = LLONG_MIN;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+			m[a[i]]++;
+			mn = max(mn, m[a[i]]);
+		}
+		if (n <= 2) {
+			cout << 0 << "\n";
+		} else {
+			cout << min(n - mn,n-2)<< "\n";
+		}
+	}
+
+	return 0;
+}
+
+// remove online judge snippet for Google Competitions
