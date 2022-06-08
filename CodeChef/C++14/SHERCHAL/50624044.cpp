9323465a39d7469486ee23dd6be70092f13be596
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:26 +0530

    Committing files

---
+++ CodeChef/C++14/SHERCHAL/50624044.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define pb push_back
+#define fi first
+#define se second
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e17;
+
+signed main() {
+
+	NFS;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		string s; cin >> s;
+		map<char, int> m;
+		int mn = 1e15;
+		for (int i = 0; i < n; i++) {
+			m[s[i]]++;
+		}
+		string a = "sherlock";
+		for (int i = 0; i < 8; i++) {
+			mn = min(mn, m[a[i]]);
+		}
+		int ans = mn;
+		cout << ans << "\n";
+	}
+
+	return 0;
+}
+// remove online judge snippet for Google Code Jam
