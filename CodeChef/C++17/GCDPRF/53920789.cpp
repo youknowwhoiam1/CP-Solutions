f27c5a8ed4dea0785b547ae1e2d3fb322edbdff3
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:04 +0530

    Committing files

---
+++ CodeChef/C++17/GCDPRF/53920789.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define pb push_back
+#define fi first
+#define se second
+#define max3(a, b, c) max(max((a), (b)), (c))
+#define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
+#define min3(a, b, c) min(min((a), (b)), (c))
+#define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e17;
+
+int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+int mod_mul(int a, int b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+int pwr(int a, int b) {a %= mod; int res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a % b);}
+
+signed main() {
+
+	NFS;
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n; cin >> n;
+		int a[n];
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		int  p = a[0];
+		bool ok = true;
+		for (int i = 0; i < n; i++) {
+			p = gcd(p, a[i]);
+			if (p != a[i]) {
+				ok = false;
+				break;
+			}
+		}
+		if (ok) {
+			for (int i = 0; i < n; i++) {
+				cout << a[i] << " ";
+			} cout << "\n";
+		} else {
+			cout << -1 << "\n";
+		}
+	}
+
+	return 0;
+}
