5a6a4ab076775872bead15153b91bf159d5b8cef
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:25:09 +0530

    Committing files

---
+++ CodeChef/C++14/ICM0013/44690205.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <string>
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define int long long
+using namespace std;
+ 
+signed main() {
+     fio;
+     int t;
+     cin>>t;
+     while(t--)
+     {
+          int n;
+          cin>>n;
+          cout<<n<<endl;
+     }
+     return 0;
+}
