9f977279854a367a706382579576a1f17717f17a
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:11 +0530

    Committing files

---
+++ CodeChef/C++17/RATRIUNIQUE/53129642.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define pb push_back
+#define fi first
+#define se second
+#define max3(a, b, c) max(max((a), (b)), (c))
+#define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
+#define min3(a, b, c) min(min((a), (b)), (c))
+#define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e17;
+
+int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+int inv(int i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+
+signed main() {
+
+    NFS;
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n, k, cnt = 0;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            if (m[a[i]] == 0 and a[i] > k) {
+                cnt++;
+            }
+            m[a[i]]++;
+        }
+        cout << cnt << "\n";
+    }
+
+    return 0;
+}
