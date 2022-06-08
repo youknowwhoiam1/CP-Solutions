741b039e8a1b65d14f29eb264de237d7e89f8d9c
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:26:26 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/38837183.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+     int t=1;
+     while(t>0){
+          int x;
+          cin >> x;
+          if(x==42){
+               break;
+          }else
+          {
+               cout << x << endl;
+          }
+     }
+     
+
+	 
+	
+	
+
+	return 0;
+}
