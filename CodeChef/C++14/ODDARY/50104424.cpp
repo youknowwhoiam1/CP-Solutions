a06449426878caa4ed2467e8705740ab3b92173d
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:40 +0530

    Committing files

---
+++ CodeChef/C++14/ODDARY/50104424.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e17;
+const int N = 1e6 + 1;
+
+// #undef assert
+// #define assert(x) if(!(x))while(true);
+
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+signed main() {
+
+    NFS;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+    int ans[1001];
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 1000; j++) {
+            int idx = pow(2, i - 1) * (2 * j - 1);
+            if (idx > 1000) {
+                break;
+            } else {
+                ans[idx] = i;
+            }
+        }
+    }
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cout << ans[i] << " ";
+        } cout << "\n";
+    }
+
+    return 0;
+}
+
+// remove online judge snippet for Google Competitions
