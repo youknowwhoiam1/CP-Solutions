312ce0ad0acce6cd37526a6360605f2d2fdc8f9f
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2022-06-08 20:24:37 +0530

    Committing files

---
+++ CodeChef/C++14/JAG/50235468.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <string>
+#define int long long
+#define NFS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define pb push_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end() //Forward traversal
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define max4(a,b,c,d) max(max((a),(b)),max((c),(d)))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define min4(a,b,c,d) min(min((a),(b)),min((c),(d)))
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e17;
+const int N = 2 * 1e5 + 1;
+
+// #undef assert
+// #define assert(x) if(!(x))while(true);
+
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+signed main() {
+
+	NFS;
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int a[n];
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		vector<int> v;
+		for (int i = 0; i < n; i++) {
+			v.push_back(a[i] - i);
+		}
+		sort(all(v));
+		int cnt = n;
+		for(int i = 0; i <n-1;i++){
+		     if(v[i] != v[i+1]){
+		          cnt--;
+		     }
+		}
+		cout<<cnt<<"\n";
+	}
+
+	return 0;
+}
